99f64d539e432fd9130de96eb778dd40dfceb2f0
biniyam yoseph <biniyamyoseph063@gmail.com>
2025-09-04 06:51:21 +0300

    font and other setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="h1-bold">Home</div>
+      <div className="h1-bold font-space-grotesk">Home</div>
+    </div>
+  );
 };
 
 export default Home;
